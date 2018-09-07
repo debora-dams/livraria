6db2476fc9f6f9f2c33a37a730f43295953a5e1d
debora-dams <deboraaurea7@gmail.com>
2018-09-07 11:35:21 -0300

    adição do atributo nome a classe Autor.java

--- demo/src/main/java/ifal/edu/pw2/demo/model/Autor.java
+++ demo/src/main/java/ifal/edu/pw2/demo/model/Autor.java
@@ -1,17 +1,42 @@
 package ifal.edu.pw2.demo.model;
 
 import javax.persistence.Column;
-import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
-// Marca a classe compositora com a anotacao
-@Embeddable
+@Entity
 public class Autor {
 
+	@Id
+	@GeneratedValue
+	private Integer idAutor;
 	@Column
 	private String nome;
+	@OneToOne
+	@JoinColumn(name = "idLivro")
+	private Livro livro;
 
 	public Autor() {
 
+	}
+
+	public Integer getIdAutor() {
+		return idAutor;
+	}
+
+	public void setIdAutor(Integer idAutor) {
+		this.idAutor = idAutor;
+	}
+
+	public Livro getLivro() {
+		return livro;
+	}
+
+	public void setLivro(Livro livro) {
+		this.livro = livro;
 	}
 
 	public String getNome() {
@@ -26,6 +51,8 @@ public class Autor {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((idAutor == null) ? 0 : idAutor.hashCode());
+		result = prime * result + ((livro == null) ? 0 : livro.hashCode());
 		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
 		return result;
 	}
@@ -39,6 +66,16 @@ public class Autor {
 		if (getClass() != obj.getClass())
 			return false;
 		Autor other = (Autor) obj;
+		if (idAutor == null) {
+			if (other.idAutor != null)
+				return false;
+		} else if (!idAutor.equals(other.idAutor))
+			return false;
+		if (livro == null) {
+			if (other.livro != null)
+				return false;
+		} else if (!livro.equals(other.livro))
+			return false;
 		if (nome == null) {
 			if (other.nome != null)
 				return false;
